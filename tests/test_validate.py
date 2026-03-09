bac364c1543d908dda976d2583b67c951475644a
Penchala Prasad <penchalad09-max@example.com>
2026-03-09 21:27:09 +0530

    Fix Pytest  Issues for CI pipeline

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -5,9 +5,11 @@ from python.validate_app import validate_app
 def test_validate_app(capsys):
     with patch("requests.get") as mock_get:
         mock_get.return_value.status_code = 200
+        mock_get.return_value.text = "Hello World"  # or whatever your app returns
         validate_app("http://localhost:8081")
         captured = capsys.readouterr()
         assert "✅ App is serving content correctly." in captured.out
+
 
 
 
